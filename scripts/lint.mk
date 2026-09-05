ac78b020de0a97a82b464775335d069cefbd1d9d
aler9 <46489434+aler9@users.noreply.github.com>
2026-09-05 11:09:22 +0200

    add prettier

--- scripts/lint.mk
+++ scripts/lint.mk
@@ -1,4 +1,11 @@
-lint:
+lint-go:
 	docker run --rm -v $(shell pwd):/app -w /app \
 	$(LINT_IMAGE) \
 	golangci-lint run -v
+
+lint-other:
+	echo "$$DOCKERFILE_PRETTIER" | docker build . -f - -t temp
+	docker run --rm -v "$(shell pwd)/:/s" -w /s temp \
+	sh -c "prettier --check ."
+
+lint: lint-go lint-other
